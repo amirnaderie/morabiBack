8f19327ddbbd0639e60ef6ee2104acd42eb9a05f
armanamiri <armanlegand1396@gmail.com>
2024-10-27 08:27:49 +0330

    feat: add env for development and read app port from env

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  const config = app.get<ConfigService>(ConfigService);
+  const appPort = config.get('APP_PORT');
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,8 +18,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
-  logger.log('Server running on port 3000');
+  await app.listen(appPort);
+  logger.log(`Server running on port ${appPort}`);
 }
 bootstrap();
