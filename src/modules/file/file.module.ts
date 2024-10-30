35a7830372048c32831adb8beff9c31c077ae1b0
Amir Naderi <amirnaderie@gmail.com>
2024-10-30 15:25:57 +0330

    14030809_02

--- src/modules/file/file.module.ts
+++ src/modules/file/file.module.ts
@@ -6,9 +6,10 @@ import { File } from './entities/file.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UtilityModule } from 'src/utility/utility.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([File]),UtilityModule, ConfigModule],
+  imports:[TypeOrmModule.forFeature([File]),UtilityModule, ConfigModule,AuthModule],
   controllers: [FileController],
   providers: [FileService],
 })
