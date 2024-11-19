eb278eb908c76c1f38e8ffc06ef022fb4d221cd2
Amir Naderi <amirnaderie@gmail.com>
2024-11-19 18:58:53 +0330

    14030829_02

--- src/modules/plan/plan.controller.ts
+++ src/modules/plan/plan.controller.ts
@@ -7,34 +7,47 @@ import {
   Param,
   Delete,
   Req,
+  SetMetadata,
+  UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreatePlanDto } from './dto/create-plan.dto';
 import { UpdatePlanDto } from './dto/update-plan.dto';
 import { PlanService } from './providers/plan.service';
 import { User } from '../users/entities/user.entity';
 import { GetUser } from 'src/decorators/getUser.decorator';
+import { Plan } from './entities/plan.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { RolesGuard } from 'src/guards/role.guard';
+import { HttpResponseTransform } from 'src/interceptors/http-response-transform.interceptor';
 
-@Controller('plan')
+@Controller('plans')
+
+@UseGuards(AuthGuard, RolesGuard)
+@UseInterceptors(HttpResponseTransform)
 export class PlanController {
   constructor(private readonly planService: PlanService) {}
 
   @Post()
-  create(
+  @SetMetadata('permission', 'create-plan')
+  async create(
     @GetUser() user: User,
     @Body() createPlanDto: CreatePlanDto,
     @Req() req: Request,
-  ) {
-    return this.planService.create(createPlanDto, user, req);
+  ): Promise<{ data: Plan }> {
+    return await this.planService.create(createPlanDto, user, req);
   }
 
   @Get()
-  findAll() {
-    return this.planService.findAll();
+  @SetMetadata('permission', 'plans')
+  findAll(@GetUser() user: User, @Req() req: Request) {
+    return this.planService.findAll(user.id, req);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.planService.findOne(+id);
+  @SetMetadata('permission', 'plan')
+  findOne(@Param('id') id: string, @Req() req: Request) {
+    return this.planService.findOne(id, req);
   }
 
   @Patch(':id')
