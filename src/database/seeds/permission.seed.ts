8ad64224df6d13d2a449cd1feaef65659e1f3de3
armanamiri <armanlegand1396@gmail.com>
2024-12-15 10:17:31 +0330

    ref: number for permissions

--- src/database/seeds/permission.seed.ts
+++ src/database/seeds/permission.seed.ts
@@ -17,36 +17,36 @@ export const createPermissionSeed = async (AppDataSource: DataSource) => {
       .insert()
       .into(PermissionSeed)
       .values([
-        { name: 'ایجاد حرکت', enName: 'create-movement' },
+        { name: 'ایجاد حرکت', enName: 'create-movement' }, // 1
         { name: 'ویرایش حرکت', enName: 'update-movement' },
         { name: 'حذف حرکت', enName: 'delete-movement' },
         { name: 'مشاهده حرکت', enName: 'movement' },
-        { name: 'مشاهده لیست حرکات', enName: 'movements' },
+        { name: 'مشاهده لیست حرکات', enName: 'movements' }, // 5
         { name: 'ایجاد دسترسی', enName: 'create-permission' },
         { name: 'ویرایش دسترسی', enName: 'update-permission' },
         { name: 'حذف دسترسی', enName: 'delete-permission' },
         { name: 'مشاهده دسترسی', enName: 'read-permission' },
-        { name: 'مشاهده لیست دسترسی', enName: 'read-permissions' },
+        { name: 'مشاهده لیست دسترسی', enName: 'read-permissions' }, // 10
         { name: 'ایجاد نقش', enName: 'create-role' },
         { name: 'ویرایش نقش', enName: 'update-role' },
         { name: 'حذف نقش', enName: 'delete-role' },
         { name: 'مشاهده نقش', enName: 'read-role' },
-        { name: 'مشاهده لیست نقش', enName: 'read-roles' },
+        { name: 'مشاهده لیست نقش', enName: 'read-roles' }, // 15
         { name: 'تغییر دسترسی نقش', enName: 'update-role-permission' },
         { name: 'ایجاد تسک', enName: 'create-task' },
         { name: 'ویرایش تسک', enName: 'update-task' },
         { name: 'حذف تسک', enName: 'delete-task' },
-        { name: 'مشاهده لیست تسک', enName: 'read-tasks' },
+        { name: 'مشاهده لیست تسک', enName: 'read-tasks' }, // 20
         { name: 'مشاهده تسک', enName: 'task' },
         { name: 'مشاهده منوی حرکات', enName: 'movements' },
         { name: 'مشاهده حرکت', enName: 'movement' },
         { name: 'مشاهده برنامه ها', enName: 'plans' },
-        { name: 'مشاهده برنامه', enName: 'plan' },
+        { name: 'مشاهده برنامه', enName: 'plan' }, // 25
         { name: 'مشاهده منوی ورزشکاران', enName: 'athletes' },
         { name: 'مشاهده ورزشکار', enName: 'athlete' },
         { name: 'مشاهده منوی پکیج ها', enName: 'packages' },
         { name: 'مشاهده پکیج', enName: 'package' },
-        { name: 'مشاهده منوی فرم های ابتدایی', enName: 'elementary' },
+        { name: 'مشاهده منوی فرم های ابتدایی', enName: 'elementary' }, // 30
         { name: 'مشاهده منوی فرم های کنترل', enName: 'control' },
         { name: 'مشاهده تسک', enName: 'read-task' },
         {
@@ -54,19 +54,19 @@ export const createPermissionSeed = async (AppDataSource: DataSource) => {
           enName: 'create-movement-default',
         },
         { name: 'ایجاد فرم', enName: 'create-form' },
-        { name: 'مشاهده فرم', enName: 'read-form' },
+        { name: 'مشاهده فرم', enName: 'read-form' }, // 35
         { name: 'ویرایش فرم', enName: 'update-form' },
         { name: 'حذف فرم', enName: 'delete-form' },
         { name: 'مشاهده لیست فرم‌ها', enName: 'forms' },
 
         { name: 'ایجاد سوال فرم‌ها', enName: 'create-formQuestion' },
-        { name: 'مشاهده سوال فرم‌ها', enName: 'read-formQuestion' },
+        { name: 'مشاهده سوال فرم‌ها', enName: 'read-formQuestion' }, // 40
         { name: 'ویرایش سوال فرم‌ها', enName: 'update-formQuestion' },
         { name: 'حذف سوال فرم‌ها', enName: 'delete-formQuestion' },
         { name: 'مشاهده لیست سوالات فرم‌ها', enName: 'form-questions' },
 
         { name: 'ایجاد پاسخ سوال فرم‌ها', enName: 'create-formQuestionAnswer' },
-        { name: 'مشاهده پاسخ سوال فرم‌ها', enName: 'read-formQuestionAnswer' },
+        { name: 'مشاهده پاسخ سوال فرم‌ها', enName: 'read-formQuestionAnswer' }, // 45
         {
           name: 'ویرایش پاسخ سوال فرم‌ها',
           enName: 'update-formQuestionAnswer',
@@ -77,12 +77,12 @@ export const createPermissionSeed = async (AppDataSource: DataSource) => {
           enName: 'form-questions-answer',
         },
         { name: 'ایجاد برنامه', enName: 'create-plan' },
-        { name: 'ویرایش برنامه', enName: 'update-plan' },
+        { name: 'ویرایش برنامه', enName: 'update-plan' }, // 50
         { name: 'حذف برنامه', enName: 'delete-plan' },
-        {
-          name: 'مربی لیست ورزشکاران خود را ببیند',
-          enName: 'mentor-read-athletes',
-        },
+        // {
+        //   name: 'مربی لیست ورزشکاران خود را ببیند',
+        //   enName: 'mentor-read-athletes',
+        // },
       ])
       .execute();
   } catch (error) {
